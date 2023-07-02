7c6eb4b5b4153238a84b1d6cba56fd4373f51f56
Yury Chaikouski <klaidik92@mail.ru>
2023-07-02 22:19:00 +0200

    Измененния обработчика событий

--- src/menu.js
+++ src/menu.js
@@ -19,14 +19,31 @@ export class ContextMenu extends Menu {
         this.menuContainer.style.display = "none";
     }
 
+    handleMenuClick(event) {
+        const clickedElement = event.target;
+        if (clickedElement.tagName === "LI") {
+            const clickedText = clickedElement.innerText;
+            const clickedModule = this.items.find(module => module.text === clickedText);
+            
+            if (clickedModule) {
+                this.close();
+                const body = document.body;
+                body.style.backgroundColor = "#fff";
+                while (body.lastElementChild !== this.menuContainer) {
+                    body.removeChild(body.lastElementChild);
+                }
+                clickedModule.trigger();
+            }
+        }
+    }
+
     add(module) {
         this.items.push(module);
         const menuItem = document.createElement("li");
         menuItem.className = "menu-item";
         menuItem.innerText = module.text;
         menuItem.addEventListener("click", () => {
-            this.close();
-            module.trigger();
+            this.handleMenuClick(event);
         });
         this.menuContainer.appendChild(menuItem);
     }
