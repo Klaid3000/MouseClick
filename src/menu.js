978407d3fd8fa9ba93e9dd3651debc7142791677
Yury Chaikouski <klaidik92@mail.ru>
2023-07-02 22:21:13 +0200

    fixed

--- src/menu.js
+++ src/menu.js
@@ -4,31 +4,31 @@ export class ContextMenu extends Menu {
     constructor() {
         super();
         this.items = [];
-        this.menuContainer = document.querySelector("#menu");
+        this.menuContainer = document.querySelector('#menu');
     }
 
     open(x, y) {
         if (this.items.length === 0) return;
         
-        this.menuContainer.style.left = x + "px";
-        this.menuContainer.style.top = y + "px";
-        this.menuContainer.style.display = "block";
+        this.menuContainer.style.left = x + 'px';
+        this.menuContainer.style.top = y + 'px';
+        this.menuContainer.style.display = 'block';
     }
 
     close() {
-        this.menuContainer.style.display = "none";
+        this.menuContainer.style.display = 'none';
     }
 
     handleMenuClick(event) {
         const clickedElement = event.target;
-        if (clickedElement.tagName === "LI") {
+        if (clickedElement.tagName === 'LI') {
             const clickedText = clickedElement.innerText;
             const clickedModule = this.items.find(module => module.text === clickedText);
             
             if (clickedModule) {
                 this.close();
                 const body = document.body;
-                body.style.backgroundColor = "#fff";
+                body.style.backgroundColor = '#fff';
                 while (body.lastElementChild !== this.menuContainer) {
                     body.removeChild(body.lastElementChild);
                 }
@@ -39,10 +39,10 @@ export class ContextMenu extends Menu {
 
     add(module) {
         this.items.push(module);
-        const menuItem = document.createElement("li");
-        menuItem.className = "menu-item";
+        const menuItem = document.createElement('li');
+        menuItem.className = 'menu-item';
         menuItem.innerText = module.text;
-        menuItem.addEventListener("click", () => {
+        menuItem.addEventListener('click', () => {
             this.handleMenuClick(event);
         });
         this.menuContainer.appendChild(menuItem);
